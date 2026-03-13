b093dde045362b34d5fc256a0f0ad4548a3a45af
romx-dev <romariojdosantos@gmail.com>
2026-03-13 01:52:20 -0300

    Implement dashboard rendering logic
    Update the home page to correctly render the dashboard stage when a user
    is authenticated and has a database profile. This includes fetching user
    data and plan limits.

--- app/page.tsx
+++ app/page.tsx
@@ -8,33 +8,31 @@ import NotSignedInStage from "./components/stages/not-signed-in-stage";
 export default async function Home() {
   const user = await currentUser();
 
-  // if (!user) {
-  //   return <NotSignedInStage />;
-  // }
+  if (!user) {
+    return <NotSignedInStage />;
+  }
 
-  // const dbUser = await prisma.user.findUnique({
-  //   where: { clerkId: user.id },
-  //   include: { links: true },
-  // });
+  const dbUser = await prisma.user.findUnique({
+    where: { clerkId: user.id },
+    include: { links: true },
+  });
 
-  // if (!dbUser) {
-  //   return <ClaimUsernameStage />;
-  // }
+  if (!dbUser) {
+    return <ClaimUsernameStage />;
+  }
 
-  // const planLimits = getPlanLimits(dbUser.plan);
-  // const limitCheck = canCreateLink(
-  //   dbUser.plan,
-  //   dbUser.links.length,
-  //   dbUser.planExpiresAt,
-  // );
+  const planLimits = getPlanLimits(dbUser.plan);
+  const limitCheck = canCreateLink(
+    dbUser.plan,
+    dbUser.links.length,
+    dbUser.planExpiresAt,
+  );
 
   return (
-    <NotSignedInStage />
-    // <DashboardStage />
-    // dbUser={dbUser}
-    // planLimits={planLimits}
-    // limitCheck={limitCheck}
-    //
-    // />
+    <DashboardStage
+      dbUser={dbUser}
+      planLimits={planLimits}
+      limitCheck={limitCheck}
+    />
   );
 }
