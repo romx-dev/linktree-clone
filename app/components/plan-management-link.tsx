c159d5d051a73bf0bf1f4b3deecbec436b234e8f
romx-dev <romariojdosantos@gmail.com>
2026-03-13 01:51:02 -0300

    Refactor: Simplify PlanManagementLink component props

--- app/components/plan-management-link.tsx
+++ app/components/plan-management-link.tsx
@@ -4,9 +4,7 @@ type PlanManagementLinkProps = {
   plan: "FREE" | "PRO" | "PREMIUM" | string;
 };
 
-export default function PlanManagementLink({
-  plan,
-}: PlanManagementLinkProps) {
+export default function PlanManagementLink({ plan }: PlanManagementLinkProps) {
   return (
     <Link href="/planos" className="text-sm hover:underline font-semibold">
       {plan === "FREE" ? "Fazer Upgrade" : "Gerenciar Plano"}
