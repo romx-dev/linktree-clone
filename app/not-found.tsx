b49baad41c539ef63ae272b7a1975862ee53c7da
romx-dev <romariojdosantos@gmail.com>
2026-03-13 02:01:07 -0300

    Update 404 page for larger screens

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,9 @@ export default function NotFound() {
       <main className="flex w-full max-w-2xl flex-col items-center justify-center px-6 py-20">
         <div className="card">
           <div className="flex flex-col items-center gap-8 text-center w-full">
-            <h1 className="text-5xl font-bold leading-tight text-black">404</h1>
+            <h1 className="text-5xl md:text-9xl font-bold leading-tight text-black">
+              404
+            </h1>
             <p className="text-lg text-[#6B7280] max-w-md">
               Página não encontrada. A página que você está procurando não
               existe.
